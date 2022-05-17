606e47974381a13862fa296b7fbd5a05c574a4f2
anjalireghunath <anjalireghu98@gmail.com>
2022-05-17 22:47:34 +0530

    search_product

---
+++ src/components/SearchProduct.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const SearchProduct = () => {
+  return (
+    <div>
+
+<div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+                    <label for="" class="form-label">Product Name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+                    <button class="btb btn-success">Search</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default SearchProduct
